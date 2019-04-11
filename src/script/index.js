b31f910279bf47ad45092b7f4ecf7644742c56ab
Thomas Rix <trix@dr.dk>
2019-04-11 12:02:39 +0200

    Simple Grid system wit rainbow colored circles

--- src/script/index.js
+++ src/script/index.js
@@ -1,7 +1,7 @@
 import Example from "./components/example";
-import '../styles/styles.scss';
-// import '../assets/**/*.*';
+import CanvasTest from "./components/canvas-test";
 // import '../styles/styles.scss';
 
 console.log('Hello again Parcels', process.env.DEBUGGING);
-let ex = new Example();
+// let ex = new Example();
+let ct = new CanvasTest();
